ec5bf84f4790a68044a0296648066df8b7ffebac
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2023-10-09 07:52:09 +0530

    Add files via upload

---
+++ day20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    printf("enter the first 10 natural number\n");
+    
+
+    for ( i = 0; i <=10; i++)
+    {
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
